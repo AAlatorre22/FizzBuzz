171d7e3b44f63c90c162ff2a4c06e7828879697c
Alejandro Alatorre <alejandro.c.alatorre@gmail.com>
2023-04-04 09:46:06 -0700

    Created FizzBuzz

---
+++ Fizzbuzz!/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Fizzbuzz_
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Please input a number");
+            
+            int userInput = int.Parse(Console.ReadLine());
+            
+            FizzBuzz(userInput);
+
+            Console.WriteLine(FizzBuzz(userInput));
+
+
+        }
+
+            public static string FizzBuzz(int userInput)
+            {
+
+                if (userInput == null || userInput == 0)
+                {
+                    return "Invalid input";
+                }
+
+                if (userInput % 3 == 0 && userInput % 5 == 0)
+                {
+                    return "fizbuzz";
+                }
+
+                if (userInput % 3 == 0)
+                {
+                    return "fizz";
+                }
+
+                if (userInput % 5 == 0)
+                {
+                    return "buzz";
+                }
+
+
+                else
+                {
+                    return "No fizzbuzz for you :(";
+                }
+
+            }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
